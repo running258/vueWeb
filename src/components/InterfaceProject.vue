235ef8a6eea34e01f71faee503fa426cf57f6a3b
zhangleyuan <zhangleyuan@viewhigh.com>
2019-01-31 16:51:42 +0800

    1

--- src/components/InterfaceProject.vue
+++ src/components/InterfaceProject.vue
@@ -9,12 +9,19 @@
     <el-autocomplete v-model="state4" :fetch-suggestions="querySearchAsync" placeholder="项目名称查询" @select="handleSelect" clearable></el-autocomplete>
     <el-button type="primary" @click="dialogVisible = true">新建项目</el-button>
 
-    <div v-for="project in projects">{{project.name}}</div>
-
-    
+    <el-card class="box-card" v-for="(project,key) in projects">
+        <div slot="header" class="clearfix">
+            <span>{{project.value}}</span>
+            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+        </div>
+        <div v-for="o in 4" :key="o" class="text item">
+            {{'列表内容 ' + o }}
+        </div>
+    </el-card>
 
     <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
-        <span>项目名称</span><el-input v-model="newProjectName" placeholder="项目名称"></el-input>
+        <span>项目名称</span>
+        <el-input v-model="newProjectName" placeholder="项目名称"></el-input>
         <el-radio-group v-model="accessRadio">
             <el-radio :label="0">公开</el-radio>
             <el-radio :label="1">私有</el-radio>
@@ -33,13 +40,17 @@ export default {
     name: "interfaceProject",
     data() {
         return {
-            projects:[{"name":1},{"name":2}],
+            projects: [{
+                "value": 1
+            }, {
+                "value": 2
+            }],
             restaurants: [],
             state4: '',
             timeout: null,
             dialogVisible: false,
             accessRadio: 0,
-            newProjectName:''
+            newProjectName: ''
         }
     },
     methods: {
@@ -78,8 +89,10 @@ export default {
                 .catch(_ => {});
         },
         saveNewProject() {
-            this.newProjectName.push(this.projects)
-            this.dialogVisible=false
+            this.projects.push({
+                "name": this.newProjectName
+            })
+            this.dialogVisible = false
         }
     },
     mounted() {
